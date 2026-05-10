c8444fb88d9fe3d20c02021c0bc040d6c1b4cea4
Nguyen Van Sieu <217349416+nguyensieu-gmer@users.noreply.github.com>
2026-05-10 09:46:43 +0700

    Add jest module and config

--- eslint.config.js
+++ eslint.config.js
@@ -1,22 +1,31 @@
-import js from "@eslint/js";
-import globals from "globals";
-import { defineConfig } from "eslint/config";
-import eslintConfigPrettier from "eslint-config-prettier/flat";
+import js from '@eslint/js';
+import globals from 'globals';
+import { defineConfig } from 'eslint/config';
+import eslintConfigPrettier from 'eslint-config-prettier/flat';
 
 export default defineConfig([
   {
-    ignores: ["dist/**", "node_modules/**"],
+    ignores: ['dist/**', 'node_modules/**'],
   },
   {
-    files: ["**/*.{js,mjs,cjs}"],
+    files: ['**/*.{js,mjs,cjs}'],
     plugins: { js },
-    extends: ["js/recommended"],
+    extends: ['js/recommended'],
     languageOptions: { globals: { ...globals.browser, ...globals.node } },
   },
   {
     rules: {
-      "no-unused-vars": "warn",
-      "arrow-body-style": ["error", "always"],
+      'no-unused-vars': 'warn',
+      'arrow-body-style': ['error', 'always'],
+      'no-undef': 'warn',
+    },
+  },
+  {
+    files: ['**/*.js'],
+    languageOptions: {
+      globals: {
+        ...globals.jest,
+      },
     },
   },
 
